13f8699bdd0541be7f9391053cc0c1138588b778
Crowdbotics <team@crowdbotics.com>
2020-07-14 05:50:59 +0000

    Update components

--- src/features/BlankScreen38892/screens/blank.js
+++ src/features/BlankScreen38892/screens/blank.js
@@ -18,7 +18,7 @@ export class _Blank extends React.Component {
     title: "Blank".toUpperCase()
   }
 
-  state = { Input_4: "", Datepicker_5: new Date("07/14/2020"), Input_6: "" }
+  state = { Input_4: "", Input_6: "", Datepicker_5: new Date("07/14/2020") }
 
   render = () => (
     <View
@@ -153,37 +153,6 @@ export class _Blank extends React.Component {
         value={this.state.Input_4}
         onChangeText={nextValue => this.setState({ Input_4: nextValue })}
       />
-      <Datepicker
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-        date={this.state.Datepicker_5}
-        onSelect={nextValue => this.setState({ Datepicker_5: nextValue })}
-      />
       <Input
         placeholder="Number Input Placeholder"
         editable={true}
@@ -218,6 +187,37 @@ export class _Blank extends React.Component {
         }}
         value={this.state.Input_6}
         onChangeText={nextValue => this.setState({ Input_6: nextValue })}
+      />
+      <Datepicker
+        style={{
+          width: "100%",
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: 20,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontStyle: "normal",
+          fontWeight: "normal",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0
+        }}
+        date={this.state.Datepicker_5}
+        onSelect={nextValue => this.setState({ Datepicker_5: nextValue })}
       />
     </View>
   )
