e8f122077f1034497e305ed540fd2cc8f3855680
Crowdbotics <team@crowdbotics.com>
2020-07-14 06:07:06 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,5 +1,10 @@
 import axios from "axios"
 import {} from "react-native-dotenv"
+const plugin11 = axios.create({
+  baseURL:
+    "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/7307/storyboard/8491/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 const newPlugin42 = axios.create({
   baseURL:
     "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/7552/storyboard/8893/",
